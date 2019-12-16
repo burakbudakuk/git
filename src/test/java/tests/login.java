9af52fce62b1dc49302ab4375ed4a3f9d51ef8b7
Burak Budak <burakbudak6@hotmail.com>
2019-12-16 20:26:27 +0000

    changed login

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -5,6 +5,7 @@ public class login {
     public static void main(String[] args) {
         System.out.println("burak");
 
+        System.out.println("hello from intelijey");
 
         System.out.println("taha");
 
